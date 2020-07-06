5c8b192ef0e004d50451ea4b90f25206f9a8fc62
tezizzm <martezkillens@gmail.com>
2020-07-06 00:02:49 -0700

    Add project files.

---
+++ AzureDevOpsTest/HomeControllerTest.cs
@@ -0,0 +1,27 @@
+using System;
+
+using Xunit;
+
+namespace AzureDevOpsTest
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void IndexTest()
+        {
+
+        }
+
+        [Fact]
+        public void PrivacyTest()
+        {
+
+        }
+
+        [Fact]
+        public void ErrorTest()
+        {
+
+        }
+    }
+}
